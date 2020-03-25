0fdcb2f1f7d83ac6664954870bb1f637c65acf3a
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-03-25 15:42:49 +0100

    Add inertia test helpers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,51 @@
 
 namespace Tests;
 
+use Illuminate\Testing\TestResponse;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+     protected function setUp(): void
+    {
+        parent::setup();
+
+        TestResponse::macro('props', function ($key = null) {
+            $props = json_decode(json_encode($this->original->getData()['page']['props']), JSON_OBJECT_AS_ARRAY);
+
+            if ($key) {
+                return Arr::get($props, $key);
+            }
+
+            return $props;
+        });
+
+        TestResponse::macro('assertHasProp', function ($key) {
+            Assert::assertTrue(Arr::has($this->props(), $key));
+
+            return $this;
+        });
+
+        TestResponse::macro('assertPropValue', function ($key, $value) {
+            $this->assertHasProp($key);
+
+            if (is_callable($value)) {
+                $value($this->props($key));
+            } else {
+                Assert::assertEquals($this->props($key), $value);
+            }
+
+            return $this;
+        });
+
+        TestResponse::macro('assertInertiaViewIs', function ($component) {
+            $page = json_decode(json_encode($this->original->getData()['page']), JSON_OBJECT_AS_ARRAY);
+
+            Assert::assertEquals($page['component'], $component);
+
+            return $this;
+        });
+    }
 }
