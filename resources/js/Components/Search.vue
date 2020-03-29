8f904f102f38f39688b34ef02148241e19049594
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-03-29 13:53:06 +0200

    Style search results

---
+++ resources/js/Components/Search.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="relative">
+        <form class="relative">
+            <input 
+                type="text" 
+                placeholder="Find topic" 
+                v-model="form.query"
+                class="block w-full text-sm text-gray-700 rounded-md shadow-md p-4 focus:outline-none focus:shadow-outline"
+            >
+            <div 
+                class="absolute right-0 mr-4 transform -translate-y-1/2"
+                style="top: 50%;" 
+            >
+                <Icon name="search" class="w-4 h-4 text-blue-800" />
+            </div>
+        </form>
+
+        <ul class="absolute bg-white w-full rounded-md shadow-md mt-2" style="top: 100%;">
+            <li 
+                v-for="result in results" 
+                :key="result.title"
+                class="text-left" 
+            >
+                <inertia-link 
+                    :href="result.url"
+                    class="flex items-center w-full px-4 py-3 hover:bg-gray-100 focus:outline-none focus:shadow-outline"
+                >
+                    <Icon :name="getResultIcon(result.type)" class="w-4 h-4 mr-4 text-gray-600" />
+                    <span class="text-gray-900">
+                        {{ result.title }}
+                    </span>
+                </inertia-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Icon from '@/Components/Icon'
+    
+    export default {
+        components: {
+            Icon,
+        },
+
+        data() {
+            return {
+                form: {
+                    query: ''
+                },
+                results: [
+                    { title: 'My first series', url: 'https://google.com', 'type': 'series' },
+                    { title: 'My second series', url: 'https://google.com', 'type': 'series' },
+                ]
+            }
+        },
+
+        methods: {
+            getResultIcon(type) {
+                return 'queue'
+            }
+        }
+    }
+</script>
+
--- resources/js/Pages/Series/Index.vue
+++ resources/js/Pages/Series/Index.vue
@@ -3,23 +3,12 @@
         <div class="bg-indigo-100 px-4 py-16 md:py-20 lg:py-32">
             <div class="text-center md:w-3/4 md:mx-auto lg:max-w-screen-sm">
                 <h1 class="font-display text-gray-900 text-4xl font-bold leading-none mb-4 md:text-5xl">Start learning now.</h1>
+
                 <p class="text-lg text-blue-900 leading-normal mb-12 lg:text-xl lg:w-3/4 lg:mx-auto">
                     Browse all our series and lessons. We have material for beginners, industry veterans and everyone in between
                 </p>
-                <form class="relative">
-                    <input 
-                        type="text" 
-                        placeholder="Find topic" 
-                        class="block w-full text-sm text-gray-700 rounded-md shadow-md p-4 focus:outline-none focus:shadow-outline"
-                    >
-                    <button 
-                        type="submit"
-                        class="absolute right-0 mr-4 transform -translate-y-1/2" 
-                        style="top: 50%;" 
-                    >
-                        <Icon name="search" class="w-4 h-4 text-blue-800" />
-                    </button>
-                </form>
+
+                <Search />
             </div>
         </div>
     </App>
@@ -27,12 +16,12 @@
 
 <script>
     import App from '@/Layouts/App'
-    import Icon from '@/Components/Icon'
+    import Search from '@/Components/Search'
 
     export default {
         components: {
             App,
-            Icon,
+            Search,
         }
     }
 </script>
