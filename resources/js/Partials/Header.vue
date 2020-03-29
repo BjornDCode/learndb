c0b3b4ea5342e073cc4f9634cc40b4824318a6d7
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-03-29 11:44:18 +0200

    Header medium styles

--- resources/js/Partials/Header.vue
+++ resources/js/Partials/Header.vue
@@ -1,13 +1,13 @@
 <template>
-    <header>
-        <div class="flex justify-between items-center p-4">
+    <header class="md:flex md:justify-between md:items-center md:p-6">
+        <div class="flex justify-between items-center p-4 md:p-0">
             <inertia-link :href="route('home')">
                 <Logo class="block h-4" />
             </inertia-link>
             
             <button 
                 type="button" 
-                class="focus:outline-none focus:shadow-outline"
+                class="focus:outline-none focus:shadow-outline md:hidden"
                 @click="toggleMenu"
             >
                 <Icon name="menu" class="w-6 h-6 text-blue-900" />
@@ -15,24 +15,24 @@
         </div>
 
         <nav 
-            class="border-t border-b border-gray-300"
+            class="border-t border-b border-gray-300 md:flex md:border-0"
             :class="{ 'hidden': !menuOpen }"
         >
             <inertia-link 
                 :href="route('library')" 
-                class="block text-sm text-blue-800 font-medium leading-none px-4 py-3"
+                class="block text-sm text-blue-800 font-medium leading-none px-4 py-3 md:inline md:p-0 ml-6"
             >
                 Library
             </inertia-link>
             <inertia-link 
                 href="#" 
-                class="block text-sm text-blue-800 font-medium leading-none px-4 py-3"
+                class="block text-sm text-blue-800 font-medium leading-none px-4 py-3 md:inline md:p-0 ml-6"
             >
                 Profile
             </inertia-link>
             <inertia-link 
                 href="#" 
-                class="block text-sm text-blue-800 font-medium leading-none px-4 py-3"
+                class="block text-sm text-blue-800 font-medium leading-none px-4 py-3 md:inline md:p-0 ml-6"
             >
                 Log out
             </inertia-link>
