bcd70b5cd08a7d0cf32c521cef9a005efa6608c0
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-03-30 14:27:47 +0200

    Style single series card

--- resources/js/Components/SeriesCard.vue
+++ resources/js/Components/SeriesCard.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-        {{ series.title }}
+    <div class="shadow-md rounded-md overflow-hidden">
+        <img 
+            :src="series.image" 
+            :alt="series.title"
+            class="block w-full" 
+        >
+        <div class="p-6">
+            <h3 class="font-display text-2xl font-medium text-gray-900 mb-2">
+                {{ series.title }}
+            </h3>
+            <p class="text-gray-600 leading-relaxed">
+                {{ series.description }}
+            </p>
+        </div>
     </div>
 </template>
 
