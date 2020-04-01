59a192df5d586a1f302eff34c35eae04dfc838bd
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-04-01 15:09:22 +0200

    Extract sidebar into seperat component

--- resources/js/Pages/Lesson/Show.vue
+++ resources/js/Pages/Lesson/Show.vue
@@ -1,41 +1,7 @@
 <template>
     <App>
         <div class="md:flex">
-            <aside class="bg-gray-100 md:sticky md:top-0 md:h-screen">
-                <button
-                    type="button"
-                    class="flex items-center w-full text-left px-4 py-2 focus:outline-none focus:shadow-outline"
-                    @click="toggleSidebar"
-                >
-                    <Icon
-                        :name="sidebarOpen ? 'cheveron-down' : 'cheveron-right'"
-                        class="w-4 h-4 mr-2 text-gray-700"
-                    />
-                    <span
-                        class="text-lg text-gray-800 font-display font-medium"
-                    >
-                        Content
-                    </span>
-                </button>
-
-                <ul v-if="sidebarOpen">
-                    <li>
-                        <inertia-link href="#">
-                            <div>
-                                <span>What is a database?</span>
-                                <div>
-                                    <div>
-                                        <span>Video</span>
-                                    </div>
-                                    <div>
-                                        <span>1 min</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </inertia-link>
-                    </li>
-                </ul>
-            </aside>
+            <Sidebar />
 
             <main>
                 // Content
@@ -46,24 +12,12 @@
 
 <script>
     import App from '@/Layouts/App'
-    import Icon from '@/Components/Icon'
+    import Sidebar from '@/Pages/Lesson/Sidebar'
 
     export default {
         components: {
             App,
-            Icon,
-        },
-
-        data() {
-            return {
-                sidebarOpen: false,
-            }
-        },
-
-        methods: {
-            toggleSidebar() {
-                this.sidebarOpen = !this.sidebarOpen
-            },
+            Sidebar,
         },
     }
 </script>
---
+++ resources/js/Pages/Lesson/Sidebar.vue
@@ -0,0 +1,57 @@
+<template>
+    <aside class="bg-gray-100 md:sticky md:top-0 md:h-screen">
+        <button
+            type="button"
+            class="flex items-center w-full text-left px-4 py-2 focus:outline-none focus:shadow-outline"
+            @click="toggle"
+        >
+            <Icon
+                :name="open ? 'cheveron-down' : 'cheveron-right'"
+                class="w-4 h-4 mr-2 text-gray-700"
+            />
+            <span class="text-lg text-gray-800 font-display font-medium">
+                Content
+            </span>
+        </button>
+
+        <ul v-if="open">
+            <li>
+                <inertia-link href="#">
+                    <div>
+                        <span>What is a database?</span>
+                        <div>
+                            <div>
+                                <span>Video</span>
+                            </div>
+                            <div>
+                                <span>1 min</span>
+                            </div>
+                        </div>
+                    </div>
+                </inertia-link>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<script>
+    import Icon from '@/Components/Icon'
+
+    export default {
+        components: {
+            Icon,
+        },
+
+        data() {
+            return {
+                open: false,
+            }
+        },
+
+        methods: {
+            toggle() {
+                this.open = !this.open
+            },
+        },
+    }
+</script>
