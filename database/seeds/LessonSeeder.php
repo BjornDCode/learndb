6778f927388e985d6c0d2a6d60608c186c2179eb
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-04-02 18:28:36 +0200

    Update LessonSeeder.php

--- database/seeds/LessonSeeder.php
+++ database/seeds/LessonSeeder.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Quiz;
+use App\Video;
 use App\Lesson;
+use App\Option;
 use App\Series;
+use App\Article;
+use App\Question;
 use Illuminate\Database\Seeder;
 
 class LessonSeeder extends Seeder
@@ -14,8 +19,34 @@ class LessonSeeder extends Seeder
     public function run()
     {
         Series::all()->each(function ($series) {
-            factory(Lesson::class, 3)->create([
+            factory(Lesson::class)->create([
                 'series_id' => $series->id,
+                'content_type' => Article::class,
+                'content_id' => factory(Article::class)->create()->id, 
+            ]);
+
+            factory(Lesson::class)->create([
+                'series_id' => $series->id,
+                'content_type' => Video::class,
+                'content_id' => factory(Video::class)->create()->id, 
+            ]);
+
+            factory(Lesson::class)->create([
+                'series_id' => $series->id,
+                'content_type' => Quiz::class,
+                'content_id' => factory(Quiz::class)->create()->id, 
+            ]);
+        });
+
+        Quiz::all()->each(function ($quiz) {
+            factory(Question::class, 2)->create([
+                'quiz_id' => $quiz->id,
+            ]);
+        });
+
+        Question::all()->each(function ($question) {
+            factory(Option::class, 4)->create([
+                'question_id' => $question->id,
             ]);
         });
     }
