d59f37e4ca55994bd39d683e861a0ded811c7407
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-04-04 13:59:38 +0200

    Create VideoPlayer

---
+++ resources/js/Components/VideoPlayer.vue
@@ -0,0 +1,40 @@
+<template>
+    <iframe
+        :src="source"
+        :width="width"
+        :height="height"
+        frameborder="0"
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+    ></iframe>
+</template>
+
+<script>
+    import Player from '@vimeo/player'
+
+    export default {
+        components: {
+            Player,
+        },
+
+        props: {
+            source: {
+                type: String,
+                required: true,
+            },
+            width: {
+                type: Number,
+                required: true,
+            },
+            height: {
+                type: Number,
+                required: true,
+            },
+        },
+
+        mounted() {
+            this.player = new Player(this.$el)
+        },
+    }
+</script>
