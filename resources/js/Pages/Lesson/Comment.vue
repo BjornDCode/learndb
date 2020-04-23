167f4255ef7fe7d5c580cff774d2332fae329fe0
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-04-23 18:13:59 +0200

    Display nested comments

--- resources/js/Pages/Lesson/Comment.vue
+++ resources/js/Pages/Lesson/Comment.vue
@@ -1,26 +1,30 @@
 <template>
-    <div class="flex items-center">
-        <div class="w-12 h-12 overflow-hidden rounded-full mr-4">
-            <img :src="image" :alt="comment.author.name" />
-        </div>
-        <div class="flex-1">
-            <div class="bg-white rounded shadow p-6">
-                <span class="text-gray-700">
-                    {{ comment.content }}
-                </span>
+    <div>
+        <div class="flex items-center">
+            <div class="w-12 h-12 overflow-hidden rounded-full mr-4">
+                <img :src="image" :alt="comment.author.name" />
             </div>
-            <div class="flex items-center justify-between px-6 py-2">
-                <div class="flex items-center">
-                    <span class="text-sm text-gray-600">
-                        {{ comment.created_at }} days ago by
-                        {{ comment.author.name }}
+            <div class="flex-1">
+                <div class="bg-white rounded shadow p-6">
+                    <span class="text-gray-700">
+                        {{ comment.content }}
                     </span>
                 </div>
-                <button class="text-gray-600 text-sm">
-                    Reply
-                </button>
+                <div class="flex items-center justify-between px-6 py-2">
+                    <div class="flex items-center">
+                        <span class="text-sm text-gray-600">
+                            {{ comment.created_at }} days ago by
+                            {{ comment.author.name }}
+                        </span>
+                    </div>
+                    <button class="text-gray-600 text-sm">
+                        Reply
+                    </button>
+                </div>
             </div>
         </div>
+
+        <slot name="footer" :children="comment.children" />
     </div>
 </template>
 
--- resources/js/Pages/Lesson/CommentsList.vue
+++ resources/js/Pages/Lesson/CommentsList.vue
@@ -1,7 +1,13 @@
 <template>
     <ul>
         <li v-for="comment in comments" :key="comment.id" class="mb-4">
-            <Comment :comment="comment" />
+            <Comment :comment="comment">
+                <template #footer="{ children }">
+                    <div class="ml-12" v-if="children.length">
+                        <CommentsList :comments="children" />
+                    </div>
+                </template>
+            </Comment>
         </li>
     </ul>
 </template>
