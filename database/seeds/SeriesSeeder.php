a6c83dad1398096f80736b528615966728017911
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-06-02 11:35:42 +0200

    Add images

--- database/seeds/SeriesSeeder.php
+++ database/seeds/SeriesSeeder.php
@@ -15,37 +15,43 @@ class SeriesSeeder extends Seeder
         factory(Series::class)->create([
             'title' => 'Database Normalisation',
             'slug' => 'database-normalisation',
-            'description' => 'Learn how avoid data anomalies in a database.'
+            'description' => 'Learn how avoid data anomalies in a database.',
+            'image' => '/storage/series_images/database-normalisation.png',
         ]);
 
         factory(Series::class)->create([
             'title' => 'ER Diagrams',
             'slug' => 'er-diagrams',
-            'description' => 'ER diagrams are an essential tool to create data models for applications. Learn how to create them, when to use them and all the different notation styles.'
+            'description' => 'ER diagrams are an essential tool to create data models for applications. Learn how to create them, when to use them and all the different notation styles.',
+            'image' => '/storage/series_images/er-diagrams.png',
         ]);
 
         factory(Series::class)->create([
             'title' => 'PostgreSQL',
             'slug' => 'postgresql',
-            'description' => 'Postgres is one of the most popular DBMS\'s. It\'s a relational database system that has powerful features like search built in.'
+            'description' => 'Postgres is one of the most popular DBMS\'s. It\'s a relational database system that has powerful features like search built in.',
+            'image' => '/storage/series_images/postgresql.png',
         ]);
 
         factory(Series::class)->create([
             'title' => 'PHP and databases',
             'slug' => 'php-and-databases',
-            'description' => 'PHP is the backend language for 80% of websites. Despite it\'s popularity it has some serious security flaws that has to be handled when interacting with a database.'
+            'description' => 'PHP is the backend language for 80% of websites. Despite it\'s popularity it has some serious security flaws that has to be handled when interacting with a database.',
+            'image' => '/storage/series_images/php-and-databases.png',
         ]);
 
         factory(Series::class)->create([
-            'title' => 'Relational database',
+            'title' => 'Relational databases',
             'slug' => 'relational-databases',
-            'description' => 'Relational databases is the old-school way of creating database systems. And there is a reason for that. 50 years after it\'s invention it\'s still very solid.'
+            'description' => 'Relational databases is the old-school way of creating database systems. And there is a reason for that. 50 years after it\'s invention it\'s still very solid.',
+            'image' => '/storage/series_images/relational-databases.png',
         ]);
 
         factory(Series::class)->create([
             'title' => 'SQL',
             'slug' => 'sql',
-            'description' => 'SQL is the query language for relational databases. Whether you are using MySQL or Postgres you\'ll still need to learn SQL to retrieve stored data.'
+            'description' => 'SQL is the query language for relational databases. Whether you are using MySQL or Postgres you\'ll still need to learn SQL to retrieve stored data.',
+            'image' => '/storage/series_images/sql.png',
         ]);
     }
 }
