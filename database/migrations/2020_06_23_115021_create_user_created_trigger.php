027ea079da3c4dad1173f1394fe081853539ea3e
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-06-23 14:55:35 +0200

    Add procedures to migrations

--- database/migrations/2020_06_23_115021_create_user_created_trigger.php
+++ database/migrations/2020_06_23_115021_create_user_created_trigger.php
@@ -14,6 +14,18 @@ class CreateUserCreatedTrigger extends Migration
      */
     public function up()
     {
+        DB::statement("CREATE OR REPLACE FUNCTION public.store_user_created_activity()
+             RETURNS trigger
+             LANGUAGE plpgsql
+            AS \$function\$
+            BEGIN
+                INSERT INTO activities (user_id, item_id, item_type, type, created_at, updated_at)
+                VALUES (NEW.id, NEW.id, 'App\User', 'created', NOW(), NOW());
+
+                RETURN NUll;
+            END
+            \$function\$
+        ");
         DB::statement("CREATE TRIGGER user_created AFTER INSERT ON \"users\" FOR EACH ROW EXECUTE PROCEDURE store_user_created_activity();");
     }
 
---
+++ database/migrations/2020_06_23_124451_create_search_procedure.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSearchProcedure extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE OR REPLACE FUNCTION public.search_series_and_lessons(search_query text)
+ RETURNS TABLE(id bigint, title character varying, slug character varying, type text, parent_slug character varying)
+ LANGUAGE plpgsql
+AS \$function\$
+BEGIN
+    RETURN QUERY
+    SELECT lessons.id, lessons.title, lessons.slug, 'lesson' AS type, series.slug as parent_slug FROM lessons
+    INNER JOIN series
+    ON lessons.series_id = series.id
+    WHERE lessons.title LIKE concat('%', search_query, '%')
+    UNION ALL 
+    SELECT series.id, series.title, series.slug, 'series' AS type, '' as parent_slug FROM series
+    WHERE series.title LIKE concat('%', search_query, '%');
+END
+\$function\$
+");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
